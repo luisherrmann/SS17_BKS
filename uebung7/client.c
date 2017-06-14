c47ef01024fbbee92fd99b0a86f6315b7e2f022d
chusemann <c.husemann@fu-berlin.de>
2017-06-15 01:06:34 +0200

    U7: Client

--- uebung7/client.c
+++ uebung7/client.c
@@ -1,40 +1,41 @@
+//Compile with gcc -std=c11 -Wall -Wextra -pedantic -o client client.c -lrt
 #include <sys/stat.h>
 #include <sys/mman.h>
 #include <sys/types.h>
-#include <fcntl.h>
 #include <inttypes.h>
 #include <unistd.h>
 #include <strings.h>
-#include <string.h>
 
 #include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
+#include <string.h>
+#include <fcntl.h>
 #include <errno.h>
+#include <stdlib.h>
+#include <sys/mman.h>
 
 #define BUF_SIZE 500
 
 struct sharedMem{
 	int len;
-	char buf[BUF_SIZE];
+	const char *buf[BUF_SIZE];
 };
 struct sharedMem *memPtr;
 int writeToStdIn(){
-	char *inputString = malloc(BUF_SIZE);
-	int fdShared;
-	fgets(inputString,BUF_SIZE,stdin);
-	printf("%s\n",inputString);
+	int fdShared;	
 	const char *filename = "/sharedSpace";
 	fdShared = shm_open(filename,O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
 	if (ftruncate(fdShared, sizeof(struct sharedMem)) == -1)
 		return EXIT_FAILURE;
 	memPtr = mmap(NULL,sizeof(struct sharedMem), PROT_READ | PROT_WRITE, MAP_SHARED, fdShared,0);
-	size_t tempBytesWritten = 0;
-	void *buf = malloc(BUF_SIZE);
-	do {
-		tempBytesWritten += write(fdShared, buf, BUF_SIZE);	
+	//printf("%i",memPtr->len);	
+	//memPtr->len = 5;
+	//memPtr->buf= malloc(BUF_SIZE);
+	const char *bufT[BUF_SIZE];	
+	fgets(bufT,BUF_SIZE,stdin);
 	
-	} while (tempBytesWritten<sizeof(inputString));
+	strcpy(memPtr->buf,bufT);
+//	printf("%s\n",memPtr->buf);
+	
 	close(fdShared);
 	return EXIT_SUCCESS;
 }
